9fa92fc95cf48242e97c48273ef1d824fd0494bd
Truman <truman@Trumans-iMac.local>
2020-07-19 20:55:55 -0500

    Form Started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import {Switch, Route, Router} from 'react-router-dom';
+import PizzaForm from './Components/PizzaForm';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Switch>
+        <Route exact path="/" />
+        <Route path='/PizzaForm.js'>
+          <PizzaForm />
+        </Route>
+      </Switch>
     </>
   );
 };
